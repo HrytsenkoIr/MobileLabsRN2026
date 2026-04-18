679153eb666a2b699b21d4d0b68360ef9093f5f6
Iryna Hrytsenko <ipz244_gis@student.ztu.edu.ua>
2026-04-18 09:30:00 +0300

    feat: add product data and auth context

---
+++ lab5/data/products.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: '1',
+    title: 'iPhone 15',
+    price: '$999',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGe-EYxT1YwTzpJl9ExlDP-ImDAaE_WJY74g&s',
+    description: 'Новий смартфон Apple.',
+  },
+  {
+    id: '2',
+    title: 'MacBook Air',
+    price: '$1299',
+    image:
+      'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-silver-select-201810?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1633058396000',
+    description: 'Легкий та потужний ноутбук.',
+  },
+  {
+    id: '3',
+    title: 'AirPods Pro',
+    price: '$249',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStdJCqOHNztdeK4qQ8Dhv-1oTSHc-4Nv4ikw&s',
+    description: 'Бездротові навушники Apple.',
+  },
+];
